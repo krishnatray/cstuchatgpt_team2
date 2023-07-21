337bb9590dc6ad5a64eec6797e315a0073afc635
fwangboulder <fwangboulder@gmail.com>
2023-07-21 06:54:08 -0700

    keep chat history demo3

--- chatBot.py
+++ chatBot.py
@@ -34,21 +34,38 @@ def generate_response(prompt):
     messages = completions.choices[0].text
     return messages
 
+
 # Chatbot interface
 def main():
     st.title("Chatbot Interface")
     st.write("Welcome to cstuGPT")
 
-    # Input for user prompt
-    user_input = st.text_input("You: ")
+    # Initialize the chat history on the first run
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+    user_input = st.text_input("You:", key="user_input", value="", placeholder="Type your question here")
 
     if st.button("Submit"):
-        if user_input:
+        if user_input.strip():
+            # Clean the user input by removing leading and trailing spaces
+            cleaned_input = user_input.strip()
+
             # Generate response using the GPT model with potential context from JSON data
-            gpt3_response = generate_response(user_input)
+            gpt3_response = generate_response(cleaned_input)
 
-            # Display the response
-            st.text_area("Chatbot:", value=gpt3_response, height=200)
+            # Save the user input and chatbot response to chat history
+            st.session_state.chat_history.append(("You", cleaned_input))
+            st.session_state.chat_history.append(("Chatbot", gpt3_response))
 
+            # Empty the input field after submitting using a JavaScript workaround
+            st.markdown("<script>document.getElementById('user_input').value = '';</script>", unsafe_allow_html=True)
+
+
+
+    # Display the chat history
+    st.write("Chat History:")
+    for sender, message in st.session_state.chat_history:
+        st.text(f"{sender}: {message}")
 if __name__ == "__main__":
     main()

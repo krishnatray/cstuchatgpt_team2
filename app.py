612d8cf51684a219ae48ace03615d8e97f3d2165
fwangboulder <fwangboulder@gmail.com>
2023-07-21 05:55:27 -0700

    modify dotenv path

--- app.py
+++ app.py
@@ -1,54 +1,26 @@
+# app.py
+# Team 2: CSTU Chat GPT App
+# Sushil Sharma
+
 import streamlit as st
-import os
-from dotenv import load_dotenv
-import openai
-import json
+import random
+import time
 
-dotenv_path = '../.env'  #modify and  change to your correct path!
-load_dotenv(dotenv_path)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+st.title("CSTUChatgpt")
 
-# Load JSON data
-json_data = json.loads(open("data.json", "r").read())  # Replace "your_json_file.json" with the actual file path
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# Generating responses from the GPT-3 API
-def generate_response(prompt):
-    for conversation in json_data["conversations"]:
-        if prompt.lower() == conversation["user"].lower():
-            # If the user's input matches any of the user questions in the JSON data,
-            # use the corresponding bot response as the initial context for GPT-3.
-            initial_context = conversation["bot"]
-            break
-    else:
-        # If no match found in JSON data, use an empty string as initial context.
-        initial_context = ""
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    completions = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"{initial_context}\nUser: {prompt}\nBot:",
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
-    messages = completions.choices[0].text
-    return messages
-
-# Chatbot interface
-def main():
-    st.title("Chatbot Interface")
-    st.write("Welcome to cstuGPT")
-
-    # Input for user prompt
-    user_input = st.text_input("You: ")
-
-    if st.button("Submit"):
-        if user_input:
-            # Generate response using the GPT model with potential context from JSON data
-            gpt3_response = generate_response(user_input)
-
-            # Display the response
-            st.text_area("Chatbot:", value=gpt3_response, height=200)
-
-if __name__ == "__main__":
-    main()
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
---
+++ chatBot.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+import openai
+import json
+
+dotenv_path = '../.env'  #modify and  change to your correct path!
+load_dotenv(dotenv_path)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Load JSON data
+json_data = json.loads(open("data.json", "r").read())  # Replace "your_json_file.json" with the actual file path
+
+# Generating responses from the GPT-3 API
+def generate_response(prompt):
+    for conversation in json_data["conversations"]:
+        if prompt.lower() == conversation["user"].lower():
+            # If the user's input matches any of the user questions in the JSON data,
+            # use the corresponding bot response as the initial context for GPT-3.
+            initial_context = conversation["bot"]
+            break
+    else:
+        # If no match found in JSON data, use an empty string as initial context.
+        initial_context = ""
+
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"{initial_context}\nUser: {prompt}\nBot:",
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    messages = completions.choices[0].text
+    return messages
+
+# Chatbot interface
+def main():
+    st.title("Chatbot Interface")
+    st.write("Welcome to cstuGPT")
+
+    # Input for user prompt
+    user_input = st.text_input("You: ")
+
+    if st.button("Submit"):
+        if user_input:
+            # Generate response using the GPT model with potential context from JSON data
+            gpt3_response = generate_response(user_input)
+
+            # Display the response
+            st.text_area("Chatbot:", value=gpt3_response, height=200)
+
+if __name__ == "__main__":
+    main()
